ef5bc8ad8e247817f785bc891e058fa28e3f06ee
anthemwingate <anthemwingate@users.noreply.github.com>
2019-12-15 15:52:27 -0500

    supplementary docs rev

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Porfolio_2',
+    packages=['Project'],
+    version='0.0.7',
+    description='EduTrax Implementation',
+    url='https://github.com/anthemwingate/Porfolio/tree/Porfolio_2',    
+)
